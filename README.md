abda9d17c23f3e35e1d888feb896e98b884fa2bb
Charles Marquez <charles.marquez@hotmail.com>
2018-04-03 13:25:09 -0400

    README changes
    - added keysample.py

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # HW2chatBot
 
 # Created by Charles Marquez (Aykon)
+
+Requires that you create your own keys.py and contains a hw2key() & discordkey() function. Refer to keysample.py
---
+++ keysample.py
@@ -0,0 +1,12 @@
+def hw2key():
+
+    key = {
+        # Request headers
+        'Ocp-Apim-Subscription-Key': 'xxx',
+    }
+    return key
+
+def discordkey():
+    
+    key = 'xxx'
+    return key
